f3214569867d861a716fb35d6f44fa73da359b2e
DuongTranDangKhoa <126379959+DuongTranDangKhoa@users.noreply.github.com>
2023-06-14 11:40:46 +0700

    first ssda

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SpringAzureDemoApplication {
 
-	@GetMapping("/message")//dsfa
+	@GetMapping("/message")
 	public String message(){
 		return "Congrats ! your application deployed successfully in Azure Platform. !";
 	}
