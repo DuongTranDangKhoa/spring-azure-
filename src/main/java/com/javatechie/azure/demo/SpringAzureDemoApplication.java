48fb535e35ff76815fe247e06c4100eee8486f1b
DuongTranDangKhoa <126379959+DuongTranDangKhoa@users.noreply.github.com>
2023-06-22 21:21:04 +0700

    first ssda

--- src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
+++ src/main/java/com/javatechie/azure/demo/SpringAzureDemoApplication.java
@@ -5,14 +5,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController
 public class SpringAzureDemoApplication {
 
 	@GetMapping("/message")
-	public String message(){
+	public Users message(){
+		Users users = new Users("aaasdf","dfads");
+		return users;
+	}
+	@GetMapping("/")
+	public String mess(){
 		return "Congrats ! your application deployed successfully in Azure Platform. !";
 	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAzureDemoApplication.class, args);
