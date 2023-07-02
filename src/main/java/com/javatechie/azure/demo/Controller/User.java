1d75a267192a7a6cee524641a31484e1cc05aa12
DuongTranDangKhoa <126379959+DuongTranDangKhoa@users.noreply.github.com>
2023-07-02 17:42:30 +0700

    first ssda

--- src/main/java/com/javatechie/azure/demo/Controller/User.java
+++ src/main/java/com/javatechie/azure/demo/Controller/User.java
@@ -1,10 +1,19 @@
 package com.javatechie.azure.demo.Controller;
 
+import com.javatechie.azure.demo.SpringAzureDemoApplication;
 import com.javatechie.azure.demo.Users;
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class User {
-
+    @GetMapping("/select")
+    public  Users getNewUser(){
+        Users add =  new Users("khoa", "khoa");
+        return add;
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAzureDemoApplication.class, args);
+    }
 }
